333ea9df38e17ae4186c81dcd9f7580d6910c529
Igor Mukhutdinov <igor.muhutdinov@gmail.com>
2021-12-28 22:46:27 +0300

    homework_01: Finished

--- homework_01/main.py
+++ homework_01/main.py
@@ -11,12 +11,29 @@ def power_numbers():
     >>> power_numbers(1, 2, 5, 7)
     <<< [1, 4, 25, 49]
     """
+    return [i ** 2 for i in numbers]
 
 
 # filter types
 ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
+
+
+def is_prime(number: int):
+    """
+    функция, которая принимает на вход число и определяет,
+    является ли это число простым
+
+    >>> is_prime(4)
+    <<< False
+    >>> is_prime(3)
+    <<< True
+    """
+    for i in range(2, number):
+        if number % i == 0:
+            return False
+    return True
 
 
 def filter_numbers():
@@ -30,3 +47,9 @@ def filter_numbers():
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
+    if filter_type == ODD:
+        return list(filter(lambda x: x % 2 == 1, numbers))
+    elif filter_type == EVEN:
+        return list(filter(lambda x: x % 2 == 0, numbers))
+    elif filter_type == PRIME:
+        return list(filter(lambda x: is_prime(x), numbers))
