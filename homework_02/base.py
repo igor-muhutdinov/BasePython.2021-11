164f16328f43ba96ef3e7930f0727183b6ca6356
Igor Mukhutdinov <igor.muhutdinov@gmail.com>
2022-01-21 21:13:37 +0300

    homework_02: Implemented base Vehicle

--- homework_02/base.py
+++ homework_02/base.py
@@ -1,5 +1,29 @@
 from abc import ABC
+from homework_02 import exceptions
 
 
 class Vehicle(ABC):
-    pass
+    weight: float = 0
+    started: bool = False
+    fuel: float = 0
+    fuel_consumption: float = 0
+
+    def __init__(self, weight, fuel, fuel_consumption):
+        self.weight = weight
+        self.fuel = fuel
+        self.fuel_consumption = fuel_consumption
+
+    def start(self):
+        if self.started is False:
+            if self.fuel > 0:
+                self.started = True
+            else:
+                raise exceptions.LowFuelError
+
+    def move(self, distance):
+        necessary_fuel = distance * self.fuel_consumption
+        if necessary_fuel <= self.fuel:
+            self.fuel -= necessary_fuel
+        else:
+            raise exceptions.NotEnoughFuel
+
