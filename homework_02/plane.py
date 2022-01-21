8311dcc153df3ca1099e2f8c6b46cd23daae3fb2
Igor Mukhutdinov <igor.muhutdinov@gmail.com>
2022-01-21 21:21:48 +0300

    homework_02: Implemented Plane

--- homework_02/plane.py
+++ homework_02/plane.py
@@ -1,3 +1,25 @@
 """
 создайте класс `Plane`, наследник `Vehicle`
 """
+from homework_02.base import Vehicle
+from homework_02 import exceptions
+
+
+class Plane(Vehicle):
+    cargo: float = 0
+    max_cargo: float
+
+    def __init__(self, weight, fuel, fuel_consumption, max_cargo):
+        super().__init__(weight, fuel, fuel_consumption)
+        self.max_cargo = max_cargo
+
+    def load_cargo(self, cargo: float):
+        if self.cargo + cargo <= self.max_cargo:
+            self.cargo += cargo
+        else:
+            raise exceptions.CargoOverload
+
+    def remove_all_cargo(self):
+        cargo = self.cargo
+        self.cargo = 0
+        return cargo
