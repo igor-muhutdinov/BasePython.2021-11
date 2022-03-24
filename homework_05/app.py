0cb7e3303700c8a3efe89f3c64dd24356e1d20c2
Igor Mukhutdinov <igor.muhutdinov@gmail.com>
2022-03-24 20:39:22 +0300

    homework_05: Realised routes

--- homework_05/app.py
+++ homework_05/app.py
@@ -11,3 +11,20 @@
 в навигационную панель добавьте ссылки на главную страницу / и на страницу /about/ при помощи url_for
 """
 
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
